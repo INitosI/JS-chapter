724fb66ee87529d3bf3b0627e646d65c0fafdb2b
Kadurin Danylo <danil.kadurin.person@gmail.com>
2024-02-12 00:20:48 +0100

    home-work-2

---
+++ js/home-work-2.js
@@ -0,0 +1,90 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// const a = 1;
+// const b = 0;
+// const c = -3;
+// a > 0 ? console.log(true) : console.log(false);
+// b > 0 ? console.log(true) : console.log(false);
+// c > 0 ? console.log(true) : console.log(false);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// const a = 'test';
+// const b = 'qwerty';
+// const c = true;
+// a === 'test' ? console.log(true) : console.log(false);
+// b === 'test' ? console.log(true) : console.log(false);
+// c === 'test' ? console.log(true) : console.log(false);
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// const a = 1;
+// const b = 10;
+// const c = 13;
+// a > 10 ? console.log(a - 5) : console.log(a + 5);
+// b > 10 ? console.log(b - 5) : console.log(b + 5);
+// c > 10 ? console.log(c - 5) : console.log(c + 5);
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+// const month = prompt('Який зараз місяць?');
+
+// switch (Number(month)) {
+// 	case 1:
+// 		console.log('Січень');
+// 		break;
+// 	case 2:
+// 		console.log('Лютий');
+// 		break;
+// 	case 3:
+// 		console.log('Березень');
+// 		break;
+// 	case 4:
+// 		console.log('Квітень');
+// 		break;
+// 	case 5:
+// 		console.log('Травень');
+// 		break;
+// 	case 6:
+// 		console.log('Червень');
+// 		break;
+// 	case 7:
+// 		console.log('Липень');
+// 		break;
+// 	case 8:
+// 		console.log('Серпень');
+// 		break;
+// 	case 9:
+// 		console.log('Вересень');
+// 		break;
+// 	case 10:
+// 		console.log('Жовтень');
+// 		break;
+// 	case 11:
+// 		console.log('Листопад');
+// 		break;
+// 	case 12:
+// 		console.log('Грудень');
+// 		break;
+// 	default:
+// 		console.log('Не знайдено');
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+// let result;
+// const value = prompt('Введіть тризначне число');
+// if (value > 99 && value < 1000) {
+// 	const V1 = Number(value.charAt(0));
+// 	const V2 = Number(value.charAt(1));
+// 	const V3 = Number(value.charAt(2));
+// 	result = V1 + V2 + V3;
+// 	console.log(result);
+// } else {
+// 	result = 'Invalid Value';
+// 	console.log(result);
+// }
