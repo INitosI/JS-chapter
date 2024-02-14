dced49c08e9edb5d16434e2fb1ce315a4e34d61a
Kadurin Danylo <danil.kadurin.person@gmail.com>
2024-02-14 02:02:00 +0100

    1

--- js/home-work-4.js
+++ js/home-work-4.js
@@ -1,14 +1,1 @@
-function callback(question, yes, no) {
-	if (confirm(question)) {
-		yes();
-	} else {
-		no();
-	}
-}
-function coutYes() {
-	console.log('you say yes');
-}
-function coutNo() {
-	console.log('you say no');
-}
-callback('Yes or not?', coutYes, coutNo);
+
